d200ea72651453ae62c598df3605b22e044f67cd
HEM Calw <HEM Calw@PC-Oliver.fritz.box>
2018-10-19 20:19:23 +0200

    Editied Hochzaehlen

--- TestInfo/src/TestPackage/Hochzaehlen.java
+++ TestInfo/src/TestPackage/Hochzaehlen.java
@@ -1,5 +1,14 @@
 package TestPackage;
 
 public class Hochzaehlen {
-
+	
+	public static void main (String[] args){
+	
+	int zahl = 1;
+	while ( zahl <= 9 ) {
+	    System.out.println(zahl);
+	    zahl++;
+	}
+	}
 }
+	
