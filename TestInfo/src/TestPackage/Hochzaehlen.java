28af8e5ebcccee961d5b4df775dca60e15fa05ea
HEM Calw <HEM Calw@PC-Oliver.fritz.box>
2018-10-20 12:03:25 +0200

    Edited Hochzählen

--- TestInfo/src/TestPackage/Hochzaehlen.java
+++ TestInfo/src/TestPackage/Hochzaehlen.java
@@ -3,12 +3,28 @@ package TestPackage;
 public class Hochzaehlen {
 	
 	public static void main (String[] args){
+		
+		int high = (int) Math.round(Math.random()*100);
+		
 	
-	int zahl = 1;
-	while ( zahl <= 9 ) {
-	    System.out.println(zahl);
-	    zahl++;
+		for (long zahl = 1; zahl <= high; zahl++ ){
+			
+		
+				if (zahl%2 == 0){
+					
+					System.out.println("Gerade Zahl\t" + zahl);
+				}
+				
+				if (zahl%2 == 1){
+					
+					System.out.println("Ungerade Zahl\t" + zahl);
+				}
+				
+			}
+			
+			
+		}
+			
 	}
-	}
-}
+
 	
